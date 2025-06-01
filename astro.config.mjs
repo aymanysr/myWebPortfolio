ba820129964222b0dfa4a4e60e130b7127751cf6
Aimane yousr <aimaneyousr@gmail.com>
2025-06-01 23:53:58 +0100

    Add Tailwind CSS configuration and integrate with Astro for improved styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://aimaneyousr.github.io',
+    base: '/myWebPortfolio/',
+    vite: {
+      plugins: [tailwindcss()],
+    },
+  });
