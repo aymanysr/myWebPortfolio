b73999f23feb2181ba98dbbbe6222e90fa18bbd7
Aimane yousr <aimaneyousr@gmail.com>
2025-06-02 15:03:55 +0100

    Update base path in Astro configuration for consistency with repository name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://aimaneyousr.github.io',
-    base: '/MY-PORTFOLIO-CMS/',
+    base: '/myWebPortfolio/',
     vite: {
       plugins: [tailwindcss()],
     },
